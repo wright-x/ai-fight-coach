dfc4e1ba2ca38d00ddfc5e41d305aceb85bc6a8a
wright-x <rudrajain06@gmail.com>
2025-07-27 21:37:14 +0700

    CRITICAL FIX: Remove deleted logger import from utils/__init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,9 +2,7 @@
 AI Fight Coach Utilities Package
 """
 
-from .logger import logger
-
 # Don't import at module level to avoid cv2 import errors
 # Components will be imported individually when needed
 
-__all__ = ['logger'] 
+__all__ = [] 
